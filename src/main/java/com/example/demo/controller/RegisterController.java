a5cda65f66b88518600b715af81d4cd7b2d9b4c6
RoxanaCantemir <roxana_c1991@yahoo.com>
2020-07-12 15:25:54 +0300

    ok cart and register

--- src/main/java/com/example/demo/controller/RegisterController.java
+++ src/main/java/com/example/demo/controller/RegisterController.java
@@ -1,13 +1,23 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.RegisterDto;
+import com.example.demo.model.User;
 import com.example.demo.service.RegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/register")
 public class RegisterController {
     @Autowired
     private RegisterService registerService;
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public User addUser(@RequestBody RegisterDto registerDto) {
+        return registerService.addUser(registerDto);
+    }
 }
