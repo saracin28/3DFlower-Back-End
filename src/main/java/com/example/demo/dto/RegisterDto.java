3a539788197468eea793078299f7ac40c8f25edb
saracin28 <robert.saracin@yahoo.com>
2020-07-12 15:29:04 +0300

    nothing

--- src/main/java/com/example/demo/dto/RegisterDto.java
+++ src/main/java/com/example/demo/dto/RegisterDto.java
@@ -3,10 +3,8 @@ package com.example.demo.dto;
 import com.example.demo.model.User;
 
 public class RegisterDto {
-
     public String username;
     public String password;
-    public String confirmpassword;
+    public String confirmPassword;
     public String email;
-
 }
--- src/main/java/com/example/demo/service/RegisterService.java
+++ src/main/java/com/example/demo/service/RegisterService.java
@@ -1,4 +1,16 @@
 package com.example.demo.service;
 
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RegisterService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public void addUser(){
+
+    }
+
 }
